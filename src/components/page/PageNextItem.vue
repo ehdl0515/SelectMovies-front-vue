e3443f80e88dc48111545fe5c690ca13fb9905bc
KDH <ehdl0515@naver.com>
2023-11-09 15:00:32 +0900

    [feat.new] 이전, 다음 버튼 vue 추가

---
+++ src/components/page/PageNextItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bt next" id="next_page">다음</div>
+</template>
+
+<style scoped>
+.bt {
+  width: 50px;
+  padding-top: 10px;
+  font-size: 1.2rem;
+  letter-spacing: -1px;
+  cursor: pointer;
+}
+
+.bt.disabled-element {
+  pointer-events: none;
+  cursor: default;
+}
+</style>
---
+++ src/components/page/PagePrevItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bt prev" id="prev_page">이전</div>
+</template>
+
+<style scoped>
+.bt {
+  width: 50px;
+  padding-top: 10px;
+  font-size: 1.2rem;
+  letter-spacing: -1px;
+  cursor: pointer;
+}
+.bt.disabled-element {
+  pointer-events: none;
+  cursor: default;
+}
+</style>
