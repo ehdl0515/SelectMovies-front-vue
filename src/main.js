d2f83645d5e49a41a719cedada6b0d3a4dda1fcb
KDH <ehdl0515@naver.com>
2023-11-10 01:42:42 +0900

    [feat.mod] app component 추가

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // import { createApp } from 'vue';
 import { createApp } from 'vue/dist/vue.esm-bundler';
-import MovieListTitleComponent from './components/movies/MovieListTitle.vue';
+import MovieListTitle from './components/movies/MovieListTitle.vue';
 import MovieItem from './components/movies/MovieItem.vue';
 import PagePrevItem from './components/page/PagePrevItem.vue';
 import PageNextItem from './components/page/PageNextItem.vue';
 import router from './router.js';
+import PageNumberItem from "@/components/page/PageNumberItem.vue";
 
 
 // app.use(router);
@@ -12,17 +13,22 @@ import router from './router.js';
 
 const app = createApp({
 });
-app.component('movie-list-title-component', MovieListTitleComponent);
+app.component('MovieListTitle', MovieListTitle);
 app.mount('#board_list_main');
 
 
 const app2 = createApp({
 });
-app2.component('movie-item', MovieItem);
+app2.component('MovieItem', MovieItem);
 app2.mount('#board_list_content');
 
 const app3 = createApp({
 });
-app3.component('page-prev-item', PagePrevItem);
-app3.component('page-next-item', PageNextItem);
+app3.component('PagePrevItem', PagePrevItem);
+app3.component('PageNextItem', PageNextItem);
 app3.mount('#board_page_main');
+
+const app4 = createApp({
+});
+app4.component('PageNumberItem', PageNumberItem);
+app4.mount('#number-button-wrapper');
