a30b149e7bdb756220da34a48f5eee5e3ee0fe6f
KDH <ehdl0515@naver.com>
2023-11-09 14:29:26 +0900

    [feat.mod] component mount 추가

--- src/main.js
+++ src/main.js
@@ -2,21 +2,27 @@
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import MovieListTitleComponent from './components/movies/MovieListTitle.vue';
 import MovieItem from './components/movies/MovieItem.vue';
+import PagePrevItem from './components/page/PagePrevItem.vue';
+import PageNextItem from './components/page/PageNextItem.vue';
 import router from './router.js';
 
-const app = createApp({
 
-});
-
-const app2 = createApp({
-
-});
 // app.use(router);
 // app.mount('#app');
-app.component('movie-list-title-component', MovieListTitleComponent);
 
+const app = createApp({
+});
+app.component('movie-list-title-component', MovieListTitleComponent);
 app.mount('#board_list_main');
 
-app2.component('movie-item', MovieItem);
 
+const app2 = createApp({
+});
+app2.component('movie-item', MovieItem);
 app2.mount('#board_list_content');
+
+const app3 = createApp({
+});
+app3.component('page-prev-item', PagePrevItem);
+app3.component('page-next-item', PageNextItem);
+app3.mount('#board_page_main');
