020ebc0573709f775dd6f3f194e09b08e10bcfad
KDH <ehdl0515@naver.com>
2023-11-09 09:58:28 +0900

    [feat.new] component mount 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 // import { createApp } from 'vue';
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import MovieListTitleComponent from './components/movies/MovieListTitle.vue';
+import MovieItem from './components/movies/MovieItem.vue';
+import router from './router.js';
 
 const app = createApp({
 
 });
 
+const app2 = createApp({
+
+});
+// app.use(router);
+// app.mount('#app');
 app.component('movie-list-title-component', MovieListTitleComponent);
 
 app.mount('#board_list_main');
+
+app2.component('movie-item', MovieItem);
+
+app2.mount('#board_list_content');
