d22428e63243b11070f47474c6372cbb2856e6d5
KDH <ehdl0515@naver.com>
2023-11-10 01:43:31 +0900

    [feat.new] pageNumberItem vue 추가

---
+++ src/components/page/PageNumberItem.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+import {GetTotalDataCnt, getTotalPageCount, setPaging} from '@/page.js';
+
+
+let currentPage;
+currentPage = 0;
+const DataCntPerPage = 10;
+const showPageCnt = 5;
+const totalPage = getTotalPageCount(await GetTotalDataCnt(), DataCntPerPage);
+let prevPage = document.getElementById("prev_page");
+let nextPage = document.getElementById("next_page");
+
+let newHtml;
+let last;
+[newHtml, last] = setPaging(currentPage, showPageCnt, totalPage)
+
+if (currentPage <= showPageCnt) {
+  prevPage.classList.add("disabled-element")
+}
+
+if ((totalPage <= showPageCnt || last === totalPage))  {
+  nextPage.classList.add("disabled-element")
+
+}
+
+</script>
+
+<template>
+  this.newHtml
+</template>
+
+<style scoped>
+
+</style>
