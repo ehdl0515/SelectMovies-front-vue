1a8fc78700464f8ac8df75fce0cdde76b7103808
KDH <ehdl0515@naver.com>
2023-11-12 13:52:55 +0900

    [feat.mod (js)] 영화 API 파라미터 추가
    - 미포함할 장르 파라미터 추가

--- src/movie_api.js
+++ src/movie_api.js
@@ -1,7 +1,11 @@
 export async function GetMoviesAll() {
-    // 211.178.126.231
-    return fetch("http://211.178.126.231/movies/all", {
-            // return fetch("http://127.0.0.1:8080/movies/all", {
+    const params = {
+        genreId: 9,
+    };
+    const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+
+    return fetch(`http://127.0.0.1:8080/movies/all?${queryString}`, {
+            // return fetch(`http://127.0.0.1:8080/movies/all?${queryString}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -13,11 +17,11 @@ export async function GetMoviesAll() {
 export async function GetMoviesAllWithPage(page) {
     const params = {
         page: page,
+        genreId: 9,
     };
 
 // URL 에 파라미터를 추가합니다.
     const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
-    // 211.178.126.231
     // return fetch(`http://127.0.0.1:8080/movies?${queryString}`, {
     return fetch(`http://211.178.126.231/movies?${queryString}`, {
             method: "GET",
